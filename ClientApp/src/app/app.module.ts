25a4520fc3367091b2d00e40263ff87ec5ebc6e0
José Eduardo <jose.eduardo1201@outlook.com>
2020-05-29 16:07:17 -0300

    modificando componentes

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -6,27 +6,27 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AlunosComponent } from './alunos/alunos.component';
 import { ProfessoresComponent } from './professores/professores.component';
-import { HomeComponent } from './home/home.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TituloComponent } from './titulo/titulo.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       NavMenuComponent,
-      HomeComponent,
       AlunosComponent,
       ProfessoresComponent,
       PerfilComponent,
-      DashboardComponent
+      DashboardComponent,
+      TituloComponent
    ],
    imports: [
-      BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+      BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
       HttpClientModule,
       FormsModule,
       RouterModule.forRoot([
-         { path: '', component: HomeComponent, pathMatch: 'full' },
+         { path: '', component: DashboardComponent, pathMatch: 'full' },
          { path: 'alunos', component: AlunosComponent },
          { path: 'professores', component: ProfessoresComponent },
          { path: 'perfil', component: PerfilComponent },
