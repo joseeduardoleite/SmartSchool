aaefe1d5b8fe95f806ed859c42008eb5bf6d72ae
José Eduardo <jose.eduardo1201@outlook.com>
2020-06-01 16:48:56 -0300

    new models

---
+++ Models/Aluno.cs
@@ -0,0 +1,22 @@
+namespace SmartSchool.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string Telefone { get; set; }
+        
+        public Aluno() { }
+
+        public Aluno(int id, string nome, string sobreNome, string telefone) 
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.SobreNome = sobreNome;
+            this.Telefone = telefone;
+               
+        }
+
+    }
+}
---
+++ Models/Professor.cs
@@ -0,0 +1,18 @@
+namespace SmartSchool.Models
+{
+    public class Professor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Disciplina { get; set; }
+
+        public Professor() { }
+
+        public Professor(int id, string nome, string disciplina) 
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Disciplina = disciplina;
+        }
+    }
+}
