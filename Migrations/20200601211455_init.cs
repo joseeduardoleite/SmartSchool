9d4df6cde6d60b52c4f3b4640ca7f47658b942dd
José Eduardo <jose.eduardo1201@outlook.com>
2020-06-01 18:32:20 -0300

    migrations init

---
+++ Migrations/20200601211455_init.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartSchool.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alunos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    SobreNome = table.Column<string>(nullable: true),
+                    Telefone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alunos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Professores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Professores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Disciplinas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    ProfessorId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Disciplinas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Disciplinas_Professores_ProfessorId",
+                        column: x => x.ProfessorId,
+                        principalTable: "Professores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AlunosDisciplinas",
+                columns: table => new
+                {
+                    AlunoId = table.Column<int>(nullable: false),
+                    DisciplinaId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AlunosDisciplinas", x => new { x.AlunoId, x.DisciplinaId });
+                    table.ForeignKey(
+                        name: "FK_AlunosDisciplinas_Alunos_AlunoId",
+                        column: x => x.AlunoId,
+                        principalTable: "Alunos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AlunosDisciplinas_Disciplinas_DisciplinaId",
+                        column: x => x.DisciplinaId,
+                        principalTable: "Disciplinas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Alunos",
+                columns: new[] { "Id", "Nome", "SobreNome", "Telefone" },
+                values: new object[,]
+                {
+                    { 1, "Ana", "Júlia", "997452512" },
+                    { 2, "Paula", "Fernandes", "999631568" },
+                    { 3, "Laura", "Souza", "997578517" },
+                    { 4, "Amanda", "Ferreira", "995658535" },
+                    { 5, "Eduardo", "José", "997416134" },
+                    { 6, "Lucas", "Alvares", "998341467" },
+                    { 7, "Mateus", "Strinberg", "995172560" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Professores",
+                columns: new[] { "Id", "Nome" },
+                values: new object[,]
+                {
+                    { 1, "Pedro" },
+                    { 2, "Roberto" },
+                    { 3, "Rodrigo" },
+                    { 4, "Luiz" },
+                    { 5, "Francisco" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Disciplinas",
+                columns: new[] { "Id", "Nome", "ProfessorId" },
+                values: new object[,]
+                {
+                    { 1, "Matemática", 1 },
+                    { 2, "Física", 2 },
+                    { 3, "Português", 3 },
+                    { 4, "Inglês", 4 },
+                    { 5, "Programação", 5 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AlunosDisciplinas",
+                columns: new[] { "AlunoId", "DisciplinaId" },
+                values: new object[,]
+                {
+                    { 2, 1 },
+                    { 4, 5 },
+                    { 2, 5 },
+                    { 1, 5 },
+                    { 7, 4 },
+                    { 6, 4 },
+                    { 5, 4 },
+                    { 4, 4 },
+                    { 1, 4 },
+                    { 7, 3 },
+                    { 5, 5 },
+                    { 6, 3 },
+                    { 7, 2 },
+                    { 6, 2 },
+                    { 3, 2 },
+                    { 2, 2 },
+                    { 1, 2 },
+                    { 7, 1 },
+                    { 6, 1 },
+                    { 4, 1 },
+                    { 3, 1 },
+                    { 3, 3 },
+                    { 7, 5 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AlunosDisciplinas_DisciplinaId",
+                table: "AlunosDisciplinas",
+                column: "DisciplinaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Disciplinas_ProfessorId",
+                table: "Disciplinas",
+                column: "ProfessorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AlunosDisciplinas");
+
+            migrationBuilder.DropTable(
+                name: "Alunos");
+
+            migrationBuilder.DropTable(
+                name: "Disciplinas");
+
+            migrationBuilder.DropTable(
+                name: "Professores");
+        }
+    }
+}
