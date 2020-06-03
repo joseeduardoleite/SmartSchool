f7463457be8d58a88ead67a9c007b93f721ef9a3
José Eduardo <jose.eduardo1201@outlook.com>
2020-06-02 23:41:51 -0300

    aluno controller

--- Controllers/AlunoController.cs
+++ Controllers/AlunoController.cs
@@ -1,27 +1,126 @@
 using System;
-using System.IO;
-using System.Linq;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SmartSchool.Data;
+using SmartSchool.Models;
 
 namespace SmartSchool.Controllers
-{
-    [ApiController]
-    [Route("api/[controller]")]
-    public class AlunoController : Controller
     {
-        [HttpGet]
-        public IActionResult Get() {
-            // return Ok("Duzer4");
-            try
+        [ApiController]
+        [Route("api/[controller]")]
+        public class AlunoController : ControllerBase
+        {
+            private readonly IRepository _repository;
+
+            public IRepository Repository { get; }
+
+            public AlunoController(IRepository repository)
             {
-                return Ok("");
+                _repository = repository;
+
             }
-            catch (Exception ex)
+
+            [HttpGet]
+            public async Task<IActionResult> Get()
             {
-                return BadRequest($"Erro: {ex.Message}");
+                try
+                {
+                    var result = await _repository.GetAllAlunosAsync(true);
+
+                    return Ok(result);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest($"Erro: {ex.Message}");
+                }
+            }
+
+            [HttpGet("{AlunoId}")]
+            public async Task<IActionResult> GetByAlunoId(int AlunoId)
+            {
+                try
+                {
+                    var result = await _repo.GetAlunoAsyncById(AlunoId, true);
+
+                    return Ok(result);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest($"Erro: {ex.Message}");
+                }
+            }
+
+            [HttpGet("ByDisciplina/{disciplinaId}")]
+            public async Task<IActionResult> GetByDisciplinaId(int disciplinaId)
+            {
+                try
+                {
+                    var result = await _repo.GetAlunosAsyncByDisciplinaId(disciplinaId, false);
+                    return Ok(result);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest($"Erro: {ex.Message}");
+                }
+            }
+
+            [HttpPost]
+            public async Task<IActionResult> post(Aluno model)
+            {
+                try
+                {
+                    _repo.Add(model);
+
+                    if (await _repo.SaveChangesAsync())
+                    {
+                        return Ok(model);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest($"Erro: {ex.Message}");
+                }
+
+                return BadRequest();
+            }
+
+            [HttpPut("{alunoId}")]
+            public async Task<IActionResult> put(int alunoId, Aluno model)
+            {
+                try
+                {
+                    var aluno = await _repo.GetAlunoAsyncById(alunoId, false);
+                    if (aluno == null) return NotFound();
+
+                    _repo.Update(model);
+
+                    if (await _repo.SaveChangesAsync())
+                    {
+                        return Ok(model);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest($"Erro: {ex.Message}");
+                }
+
+                return BadRequest();
+            }
+
+            [HttpDelete("{alunoId}")]
+            public async Task<IActionResult> delete(int alunoId)
+            {
+                try
+                {
+                    var aluno = await _repo.GetAlunoAsyncById(alunoId, false);
+                    if (aluno == null) return NotFound();
+
+                    _repo.Delete(aluno);
+
+                    if (await _repo.SaveChangesAsync())
+                    {
+                        return Ok("Deletado");
+                    }
+                }
             }
         }
-    }
-}
